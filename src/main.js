e4d47961912df90658d83b5a2cc88827c11d8633
Josh Long <josh@joshlong.com>
2022-01-04 20:19:35 -0800

    up

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from "./router";
+// import VueGtagPlugin from "vue-gtag";
+
+// app.use(VueGtagPlugin, {config: {id: 'UA-1755059-2'}})
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
